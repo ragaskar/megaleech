624d82556a58bfcaec296b3b7346afcec24005be
ragaskar <ragaskar@gmail.com>
2010-08-08 01:49:50 -0400

    Pull out client

--- lib/megaleech/controllers/downloads_controller.rb
+++ lib/megaleech/controllers/downloads_controller.rb
@@ -1,12 +1,17 @@
 module Megaleech
   class DownloadsController
+    def initialize(options = {})
+      @port = options[:port]
+      @user = options[:user]
+      @download_root = options[:destination]
+    end
     def run
       torrent = Megaleech::Torrent.next_download
       return unless torrent
       source = File.join(Megaleech.download_directory, torrent.destination)
-      destination = File.join(Megaleech.client_download_directory, torrent.destination)
-      system "ssh -p #{Megaleech.client_port} #{Megaleech.client_user}@localhost \"mkdir -p \\\"#{destination}\\\"\""
-      result = system "rsync -r --bwlimit 3000 --partial --rsh=\"ssh -p #{Megaleech.client_port}\" \"#{source}\" \"#{Megaleech.client_user}@localhost:#{destination.gsub(" ", "\\ ")}\""
+      destination = File.join(@download_root, torrent.destination)
+      system "ssh -p #{@port} #{@user}@localhost \"mkdir -p \\\"#{destination}\\\"\""
+      result = system "rsync -r --bwlimit 3000 --partial --rsh=\"ssh -p #{@port}\" \"#{source}\" \"#{@user}@localhost:#{destination.gsub(" ", "\\ ")}\""
       if (result)
         torrent.status = Megaleech::Torrent::FINISHED
         torrent.save
